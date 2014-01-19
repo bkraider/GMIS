dc310fd41f8e72a0ab33fccedff0c79a68c814f0
bhanu korremula <bkraider@gmail.com>
2014-01-19 17:43:37 -0500

    added additional properties to minimize code

--- GMIS.UnitTesting/GMIS.UnitTests/CalculatorTestFixture.vb
+++ GMIS.UnitTesting/GMIS.UnitTests/CalculatorTestFixture.vb
@@ -5,19 +5,20 @@ Imports GMIS.Business
 <TestClass()>
 Public Class CalculatorTestFixture
 
-    Private testContextInstance As TestContext
+    Private m_SystemUnderTest As Calculator
 
     '''<summary>
     '''Gets or sets the test context which provides
     '''information about and functionality for the current test run.
     '''</summary>
-    Public Property TestContext() As TestContext
+    Public ReadOnly Property SystemUnderTest() As Calculator
         Get
-            Return testContextInstance
+            If m_SystemUnderTest Is Nothing Then
+                m_SystemUnderTest = New Calculator()
+            End If
+            Return m_SystemUnderTest
         End Get
-        Set(ByVal value As TestContext)
-            testContextInstance = value
-        End Set
+
     End Property
 
 #Region "Additional test attributes"
@@ -25,53 +26,63 @@ Public Class CalculatorTestFixture
     ' You can use the following additional attributes as you write your tests:
     '
     ' Use ClassInitialize to run code before running the first test in the class
-    ' <ClassInitialize()> Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
-    ' End Sub
-    '
-    ' Use ClassCleanup to run code after all tests in a class have run
-    ' <ClassCleanup()> Public Shared Sub MyClassCleanup()
-    ' End Sub
+    <ClassInitialize()>
+    Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+    End Sub
     '
     ' Use TestInitialize to run code before running each test
-    ' <TestInitialize()> Public Sub MyTestInitialize()
-    ' End Sub
+    <TestInitialize()>
+    Public Sub MyTestInitialize()
+    End Sub
+    '
+    ' Use ClassCleanup to run code after all tests in a class have run
+    <ClassCleanup()>
+    Public Shared Sub MyClassCleanup()
+    End Sub
     '
     ' Use TestCleanup to run code after each test has run
-    ' <TestCleanup()> Public Sub MyTestCleanup()
-    ' End Sub
-    '
+    <TestCleanup()>
+    Public Sub MyTestCleanup()
+        m_SystemUnderTest = Nothing
+    End Sub
+
 #End Region
 
     <TestMethod()>
     Public Sub Add()
-        Dim calc As New Calculator
-        Dim result As Integer = 0
-        result = calc.Add(2, 3)
-        Assert.AreEqual(result, 5)
+        'Dim calc As New Calculator
+        Dim expected As Integer = 5
+        Dim actual As Integer = 0
+        actual = SystemUnderTest.Add(2, 3)
+        Assert.AreEqual(expected, actual, "Did not get the right value from the add method")
     End Sub
 
     <TestMethod()>
     Public Sub Subtract()
-        Dim calc As New Calculator
-        Dim result As Integer = 0
-        result = calc.Subtract(3, 2)
-        Assert.AreEqual(result, 1)
+        'Dim calc As New Calculator
+        Dim expected As Integer = 2
+        Dim actual As Integer = 0
+        actual = SystemUnderTest.Subtract(3, 2)
+        Assert.AreEqual(expected, actual, "Did not get the right value from the subtract method")
     End Sub
 
     <TestMethod()>
     Public Sub Multiply()
-        Dim calc As New Calculator
-        Dim result As Integer = 0
-        result = calc.Multiply(2, 3)
-        Assert.AreEqual(result, 6)
+        'Dim calc As New Calculator
+        Dim expected As Integer = 5
+        Dim actual As Integer = 0
+        actual = SystemUnderTest.Multiply(2, 3)
+        Assert.AreEqual(expected, actual, "Did not get the right value from the multiply method")
     End Sub
 
     <TestMethod()>
     Public Sub Divide()
-        Dim calc As New Calculator
-        Dim result As Integer = 0
-        result = calc.Divide(3, 3)
-        Assert.AreEqual(result, 1)
+        'Dim calc As New Calculator
+        Dim expected As Integer = 3
+        Dim actual As Integer = 0
+        actual = SystemUnderTest.Divide(9, 3)
+        Assert.AreEqual(expected, actual, "Did not get the right value from the divide method")
     End Sub
+
 
 End Class
